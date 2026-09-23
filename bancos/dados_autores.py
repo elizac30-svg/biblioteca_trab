7151f59a0257b5659f6bb26a5e2fd18c13bb296e
Eliza <eliza.c30@aluno.ifsc.edu.br>
2026-09-22 23:01:11 -0300

    a?

--- bancos/dados_autores.py
+++ bancos/dados_autores.py
@@ -1,6 +1,6 @@
 import sqlite3
-conn = sqlite3.connect("biblioteca_trab/zbiblioteca.db")
-
+conn = sqlite3.connect("biblioteca_trab/zbiblioteca.db") #obs: tá assim pq daí cria dentro da pasta,
+                                                         #e o z é pra ficar no fim e organizar.
 conn.execute("CREATE TABLE autores (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
 
 def cadastrar_autor(nome):
