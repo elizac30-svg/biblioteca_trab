56cecbd08dc223179f34e799cc3690ab07f1fae0
Eliza <eliza.c30@aluno.ifsc.edu.br>
2026-09-22 22:57:35 -0300

    aaaaaa

--- bancos/dados_livros.py
+++ bancos/dados_livros.py
@@ -1,4 +1,12 @@
 import sqlite3 
 conn = sqlite3.connect("biblioteca_trab/zbiblioteca.db")
 
-conn.execute("CREATE TABLE livros (id INTEGER PRIMAY KEY AUTOINCREMENT, )")
+#*livros*(id, titulo, autor_id, editora_id, ano_publicacao, edicao, disponivel)
+
+conn.execute("CREATE TABLE livros (id INTEGER PRIMAY KEY AUTOINCREMENT, titulo TEXT NOT NULL," \
+             "edicao INTEGER NOT NULL, disponivel BOOLEAN, ano_publicacao INTEGER" \
+             "autor_id INTEGER REFERENCES autor(id) " \
+             "editora_id INTEGER REFERENCES editora(id))")
+
+def cadastrar_livros():
+    # type: ignore
