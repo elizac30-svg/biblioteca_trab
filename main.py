5abc304fadcf6e5504d51dccdc02b343e8572e90
Eliza <eliza.c30@aluno.ifsc.edu.br>
2026-09-25 20:41:27 -0300

    a????????????????????????????????????????????????????????????????????? a

--- main.py
+++ main.py
@@ -10,10 +10,53 @@ from cadastros.dados_usuarios import cadastrar_usuarios
 
 def menu():
     while(True):
-        print("\n---- M E N U Z I N H O  de  O P Ç Õ E S  ! ! ! ----")
+        print("\n---- M E N U Z I N H O  de  O P Ç Õ E S  ! ! ! ----") #menu 0
         print("\n[1] - Opções de listas.")
         print("[2] - Opções de cadastro.")
         print("[3] - Sair )")
 
+        opcao = input("Digite a opção desejada: ")
+        #obs para Luz: Apenas um teste, modifique depois e faz o resto como quiser *figuriha q eu to pensando*
+
+        if opcao == '1':
+            while True:
+
+                print("\nmenuh lá das listinha") #menu 1
+                print("finge que existem opções aqui")
+                opcao_l = input("responde ai: ")
+            
+                if opcao_l == '1':#depois de executar volta pro mennu 1 por causa do while True:
+                    print("blablabla")
+                elif opcao_l == '2': #isso faz voltar para o menu 0
+                    break
+                else: 
+                    print("\ncoiso inavlido, ache uma bola de cristal para adivinhar as alternativas seu " \
+                    "burro, nao tem ainda pq? eoenn")
+
+        elif opcao == '2':
+            while True:
+
+                print("só pra falar q tem as opcoes lá")
+                opcao_c = input('AAAaa?: ')
+                if opcao_c == '1':
+                    break
+                else:
+                    print("banana")
+
+        elif opcao == '3':
+            break
+
+        elif opcao == '67':
+            print("\nPara se diagnostico de problemas mentais e breve internamento: " \
+            "CAPS (Centro de Atenção Psicossocial): Unidades do SUS especializadas em " \
+            "saúde mental. Você pode buscar atendimento diretamente na unidade mais próxima " \
+            "da sua casa (saiba que não é necessário um agendamento prévio para o primeiro acolhimento).")
+
+        elif opcao == '666':
+            print("\nParabéns! Você acaba de invocar Samara!")
+
+        else:
+            print("\nO SEU ACÉFALO DESPROVIDO DE INTELIGENCIA, SEU CORNO, ISSO NAO "
+                  "TA NAS OPÇÕES, SABE LER NAO O ESTRUPICIO!!!??????")
 
 menu()
